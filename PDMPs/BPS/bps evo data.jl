ac257a3cd357bf59a12ea2f667a428b3e9d0c173
Erik Raab <erikraab@gmail.com>
2026-03-03 12:29:01 +0100

    Cleaned up BPS evo data

--- PDMPs/BPS/bps evo data.jl
+++ PDMPs/BPS/bps evo data.jl
@@ -9,17 +9,33 @@ struct BPSEvoData{S, N}<:EvolutionData
     segments::S
     workspace::BPSWorkspace{N}
     gradient::Vector{Float64}
+
+    function BPSEvoData(dim::Integer)
+        #The segments
+        seg_dict = Dict{Int64, Segment{1}}(1=>Segment{1}())
+
+        #The workspace
+        fwd_position = zeros(Float64, dim)
+        st = SplitState(zeros(Float64, dim), zeros(Float64, dim), Base.RefValue{Int64}(1))
+        ada = AdaptiveData{dim}(adaptive_state = st)
+        work = BPSWorkspace(fwd_position, ada)
+
+        #The gradient
+        grad = zeros(Float64, dim)
+
+        return new{typeof(seg_dict),dim}(seg_dict, work, grad)
+    end
 end
 
-function initialize_evolution_data(pdmp::PDMP{BPS, F, D, T}, nums::NumericalParameters) where {F, D, T}
-    grad = zeros(Float64, pdmp.target.dimension)
-    seg_dict = Dict{Int64, Segment{1}}(1=>Segment{1}())
-    fwd_position = zeros(Float64, pdmp.target.dimension)
-    st = SplitState(zeros(Float64, pdmp.target.dimension), zeros(Float64, pdmp.target.dimension),Base.RefValue{Int64}(1))
-    ada = AdaptiveData{pdmp.target.dimension}(adaptive_state = st)
-    work = BPSWorkspace(fwd_position, ada)
-    return BPSEvoData(seg_dict, work, grad)
-end
+#function initialize_evolution_data(pdmp::PDMP{BPS, F, D, T}, nums::NumericalParameters) where {F, D, T}
+#    grad = zeros(Float64, pdmp.target.dimension)
+#    seg_dict = Dict{Int64, Segment{1}}(1=>Segment{1}())
+#    fwd_position = zeros(Float64, pdmp.target.dimension)
+#    st = SplitState(zeros(Float64, pdmp.target.dimension), zeros(Float64, pdmp.target.dimension),Base.RefValue{Int64}(1))
+#    ada = AdaptiveData{pdmp.target.dimension}(adaptive_state = st)
+#    work = BPSWorkspace(fwd_position, ada)
+#    return BPSEvoData(seg_dict, work, grad)
+#end
 
 function fetch_core_data!(pdmp::PDMP{BPS, F, D, T}, evo_data::BPSEvoData, numerics::NumericalParameters, state::SplitState, dyn_type::Union{DynType,TransitionType}) where {F, D, T}
     numerics.gradient!(evo_data.gradient, state.position)
